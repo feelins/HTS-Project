9d859cc7b9c20401c42afc945a4f01e636ce2bcb
feelins <feipengshao@163.com>
2019-07-09 10:26:19 +0800

    update

---
+++ scripts/Synthesis.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+""" 
+@author: shaopf
+@file: Synthesis.py 
+@version: 1.0
+@time: 2019/06/12 11:26:52
+@email: feipengshao@163.com
+@function： synthesis wav
+"""
+
+def make_unseen_model
+
